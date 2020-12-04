574e61a27490e474d433bc67e40962190a883d59
Meagan Waller <meaganewaller@gmail.com>
2020-12-04 00:36:07 -0500

    🎨 Makes the method take in different slopes
    Got part two to work!

--- day-03/puzzle.rb
+++ day-03/puzzle.rb
@@ -1,27 +1,29 @@
 class Puzzle
-  def self.count_trees(arr)
+  def self.count_trees(arr, to_increment, to_go_down=1)
     count = 0
     encountered = []
     arr.each_with_index do |v, i|
-      if i == 0
-        encountered << nil
-        count += 3
-        # As long as count is less than that the length of the line in the array
-      elsif count <= v.length - 1
-        # Add value at index to encountered array.
-        encountered << v[count]
-        # Before incrementing the count, make sure it's good to increment
-        if count + 3 <= v.length - 1
-          count += 3
+      if to_go_down == 2 && i % 2 == 0 || to_go_down == 1
+        if i == 0
+          encountered << nil
+          count += to_increment
+          # As long as count is less than that the length of the line in the array
+        elsif count <= v.length - 1
+          # Add value at index to encountered array.
+          encountered << v[count]
+          # Before incrementing the count, make sure it's good to increment
+          if count + to_increment <= v.length - 1
+            count += to_increment
+          else
+            leftover = v.length - 1 - count
+            count = to_increment - 1 - leftover
+          end
         else
           leftover = v.length - 1 - count
-          count = 2 - leftover
+          count = to_increment - 1 - leftover
+          encountered << v[count]
+          count += to_increment
         end
-      else
-        leftover = v.length - 1 - count
-        count = 2 - leftover
-        encountered << v[count]
-        count += 3
       end
     end
 
