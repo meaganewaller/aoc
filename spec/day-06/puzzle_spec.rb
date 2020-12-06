caa54619f03ea45fa0dd0070db17ddce483a01f9
Meagan Waller <meaganewaller@gmail.com>
2020-12-06 12:07:46 -0500

    ✨ Finish part two of the day 6 puzzle
    Got to use the Enumerable#tally method, super useful

--- spec/day-06/puzzle_spec.rb
+++ spec/day-06/puzzle_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 require './day-06/puzzle'
-require 'pry'
-require 'pry-nav'
 
 describe Puzzle do
   it "sums up how many yes answers there are" do
@@ -10,6 +8,24 @@ describe Puzzle do
     answers = file_data.split("\n\n")
     responses = answers.map { |a| a.split }
     expect(Puzzle.count_answers(responses)).to eql 11
+  end
+
+  it "sums up how many yes answers there are consensus on in groups" do
+    file = File.open('./spec/day-06/test_input.txt')
+    file_data = file.read
+    answers = file_data.split("\n\n")
+    responses = answers.map { |a| a.split }
+
+    expect(Puzzle.count_consensus_answers(responses)).to eql 6
+  end
+
+  xit "sums up how many yes answers there are consensus on in groups for puzzle input" do
+    file = File.open('./day-06/input.txt')
+    file_data = file.read
+    answers = file_data.split("\n\n")
+    responses = answers.map { |a| a.split }
+
+    expect(Puzzle.count_consensus_answers(responses)).to eql ___
   end
 
   xit "sums up for the puzzle input" do
